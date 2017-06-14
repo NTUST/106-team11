e013ff5b6c911aded097ca3f2a4e145eacccb4ed
Aaron <a26411277@gmail.com>
2017-06-14 23:19:21 +0800

    ManageCase

--- adventurers/static/assets/js/selectfunc.js
+++ adventurers/static/assets/js/selectfunc.js
@@ -1,4 +1,5 @@
 $(document).ready(function (){
+  var workerlist = [];
   $("#selectbtn").click(function(){
       var target = "_blank";
       var url="";
@@ -8,12 +9,16 @@ $(document).ready(function (){
       while (group.hasChildNodes()) {
         group.removeChild(group.lastChild);
       }
+      workerlist=[];
     $(".selected").each(function(){
       id++;
       $("<a"+" id=\"worker_"+ id + "\"target=\""+target+ "\"href=\"#\">&nbsp</a>").appendTo("#workergroup")
       content=$(this).find("td")[1].textContent;
       $("#worker_"+id).text(content);
+      	$("#worker_"+id).attr("href","/accounts/"+content );
       $("#worker_"+id).append("   ");
+      workerlist.push(content);
     });
+    $("#worker_hidden").val(workerlist);
   });
 });
