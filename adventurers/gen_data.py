4c9c1e4b254e85caf561bc2fc89dbef226273811
Eason Tse <shuntys92@gmail.com>
2017-06-14 18:11:16 +0800

    views

--- adventurers/gen_data.py
+++ adventurers/gen_data.py
@@ -7,6 +7,7 @@ django.setup()
 from missionboard.models import *
 from accounts.models import Member
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 import random
 from faker import Factory
@@ -31,9 +32,9 @@ def gen_users(num):
         u.save()
 
         m = Member.objects.create(user=u,
-                              level=random.randint(1, 20),
-                              bios=fake.text(),
-                              contact=fake.text())
+                                  level=random.randint(1, 20),
+                                  bios=fake.text(),
+                                  contact=fake.text())
         skill_list = []
         for x in range(1, random.randint(1, 3)):
             skill_list.append(Skill.objects.random_pick())
@@ -48,20 +49,67 @@ def gen_users(num):
 
 
 def gen_skills():
-    skill_list=['Python', 'JavaScript', 'C', 'C++', 'C#',
-                'Android', 'iOS', 'Visual Basic', 'Java',
-                'Web Design', 'Ruby', 'Graphic Design', 'Photography']
+    skill_list = ['Python', 'JavaScript', 'C', 'C++', 'C#',
+                  'Android', 'iOS', 'Visual Basic', 'Java',
+                  'Web Design', 'Ruby', 'Graphic Design', 'Photography']
     for s in skill_list:
-        skill=Skill.objects.create(name=s, description='{} 是個好程式'.format(s))
+        skill = Skill.objects.create(name=s, description='{} 是個好程式'.format(s))
     print("{} skills generated and committed to DB.".format(Skill.objects.count()))
 
 
 def gen_missions(num):
-    counter=0
+    counter = 0
     for n in range(0, num):
-        Mission.objects.generate()
+        gen_missions_helper()
         counter += 1
     print("{} missions generated and committed to DB.".format(counter))
+
+
+def gen_missions_helper():
+    u = None
+    while u is None or u is User.objects.get(id=1):
+        u = User.objects.get(id=random.randint(1, User.objects.count()))
+
+    worker_num = random.randint(2, 6)
+    level = random.randint(1, 11)
+    app_date = fake.date_time_between(
+        start_date="-14d", end_date="+7d", tzinfo=tz('Asia/Taipei'))
+
+    if app_date < timezone.now():
+        status = 'application'
+    else:
+        status = 'in_progress'
+
+    m = Mission.objects.create(
+        name=' '.join(fake.text().split(' ')[:5]),
+        posted_by=u,
+        posted_on=fake.date_time_between(
+            start_date="-30d", end_date="-15d", tzinfo=tz('Asia/Taipei')),
+        status=status,
+        application_deadline=app_date,
+        description=fake.text(),
+        working_deadline=fake.date_time_between(
+            start_date="+15d", end_date="+30d", tzinfo=tz('Asia/Taipei')),
+        required_level=level,
+        required_worker_num=worker_num
+    )
+
+    skill_list = []
+    for n in range(1, random.randint(2, 4)):
+        s = Skill.objects.random_pick()
+        if s not in skill_list:
+            skill_list.append(s)
+    m.required_skills.set(skill_list)
+    m.save()
+
+    applied_user = []
+    num = random.randint(2, 20)
+    all_users = list(User.objects.all().exclude(username='AnonymousUser'))
+
+    for x in random.sample(all_users, num):
+        ma = MissionApplication.objects.create(applied_by=x, mission=m)
+        print(x, ma)
+        x.my_profile.missions_wip.add(ma.mission)
 
 
 if __name__ == '__main__':
--- adventurers/reinitdb.sh
+++ adventurers/reinitdb.sh
@@ -5,19 +5,20 @@ cd $DIR
 
 echo 'remove files related to sqlite in django...'
 rm -v */migrations/000*
-rm db.sqlite3
+rm -v db.sqlite3
 
 source env/bin/activate
 python3 manage.py makemigrations
 python3 manage.py migrate
+python3 manage.py ckeck_permissions
 
-python gen_data.py
+python3 gen_data.py
 
-echo ''
+echo
 echo 'Setup super user...'
 python3 manage.py createsuperuser --email=admin@test.local
 
-echo ''
+echo
 echo 'Running development server...'
 python3 manage.py runserver
 exit 0
